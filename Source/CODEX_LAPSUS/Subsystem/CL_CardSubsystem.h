c64420bd7fe54bc32f5fb7cf634a03b012fd30c3
HyunJun Kim <154495800+IlLa00@users.noreply.github.com>
2026-06-02 18:31:43 +0900

    Add DT, UI
    카드선택 UI

---
+++ Source/CODEX_LAPSUS/Subsystem/CL_CardSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CL_CardSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODEX_LAPSUS_API UCL_CardSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
